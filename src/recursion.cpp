7bec9b79ba8f33548b763765558f23058708b200
Michael Francoeur <mjfrancoeur@gmail.com>
2017-01-09 19:36:09 -0500

    Completed Flood Fill function

--- src/recursion.cpp
+++ src/recursion.cpp
@@ -20,6 +20,7 @@ void doSerpinskii(GWindow &w, double leftX, double leftY, double size, int order
 double getHeight(double a, double b);
 void drawTriangle(GWindow &w, double leftX, double leftY, double size);
 int floodFill(GBufferedImage& image, int x, int y, int newColor);
+int doFloodFill(GBufferedImage& image, int x, int y, int newColor, int originalColor);
 
 
 // Returns the greatest common denominator of a and c, displaying the recursive steps to get to the gcd
@@ -55,12 +56,20 @@ void doSerpinskii(GWindow &w, double leftX, double leftY, double size, int order
         }
 }
         
-// Uses the Pythagorean theorum to return the height given two sides of the triangle
+/**
+ * Function: Get Height
+ * --------------------
+ * Returns the height of a triangle, given the hypotenuse and an adjacent side.
+ */
 double getHeight(double c, double a) {
     return sqrt(c * c - a * a);
 }
 
-
+/**
+ * Function: Draw Triangle
+ * -----------------------
+ * Draws a triangle on GWindow w
+ */
 void drawTriangle(GWindow &w, double leftX, double leftY, double size) {
     double height = getHeight(size, size / 2);
     double rightX = leftX + size;
@@ -72,10 +81,39 @@ void drawTriangle(GWindow &w, double leftX, double leftY, double size) {
 
 }
 
+/**
+ * Function: Flood Fill
+ * --------------------
+ * Calls the function Do Flood Fill to fill the selected region with a specified color.
+ * If the region selected is the same color as the specified color, the function returns 0.
+ * And does not call Do Flood Fill.
+ * Returns an integer equal to the number of pixels colored. 
+ */
 int floodFill(GBufferedImage& image, int x, int y, int newColor) {
-    // your code here
-    cout << "[recursion flood fill called]" << endl;
-    return 0;
+    int originalColor = image.getRGB(x,y);
+    if (originalColor == newColor) {
+        return 0;
+    }
+    return doFloodFill(image, x, y, newColor, originalColor);
+}
+
+/**
+ * Function: Do Flood Fill
+ * ------------------------
+ * Uses recursion to color the pixel and adjacent pixels within bounds and of the original color.
+ * Returns the number of pixels colored.
+ */
+int doFloodFill(GBufferedImage& image, int x, int y, int newColor, int originalColor) {
+    int numPixels;
+    if (image.inBounds(x, y) && image.getRGB(x, y) == originalColor) {
+        image.setRGB(x, y, newColor);
+        numPixels++;
+        doFloodFill(image, x+1, y, newColor, originalColor); // check right pixel
+        doFloodFill(image, x-1, y, newColor, originalColor); // check left pixel
+        doFloodFill(image, x, y+1, newColor, originalColor); // check pixel above
+        doFloodFill(image, x, y-1, newColor, originalColor); // check pixel below
+    }
+    return numPixels;
 }
 
 void personalCurriculum(Map<string, Vector<string>> & prereqMap,string goal) {
